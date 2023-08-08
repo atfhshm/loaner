e921ddf63f7fa58a82162f19862d471b4c043238
atfhshm <its.atfhshm@gmail.com>
2023-08-08 19:42:48 +0300

    change default permission to IsAuthenticated

--- funds/models.py
+++ funds/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+
+class Fund(models.Model):
+    # TODO: Change the on_delete action to SET_NULL after testing
+    user = models.ForeignKey(User, name="provider", on_delete=models.CASCADE, related_name="user_funds")
+
+    class Meta:
+        db_table = "funds"
+        verbose_name = "fund"
+        verbose_name_plural = "funds"
