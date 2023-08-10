9850c04e1a1424c807866c8d6af059fb70446445
atfhshm <its.atfhshm@gmail.com>
2023-08-10 11:26:51 +0300

    modify the calculate_loan_month_payment receiver to be triggered with a pre-save signal

--- loans/receivers.py
+++ loans/receivers.py
@@ -1,5 +1,5 @@
 from django.dispatch import receiver
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 
 from dateutil.relativedelta import relativedelta
 
@@ -7,27 +7,22 @@ from .models import Loan, PaymentTransaction, Amortization
 
 
 @receiver(
-    post_save,
+    pre_save,
     sender=Loan,
 )
-def calculate_loan_month_payment(sender, instance: Loan, created, **kwargs):
-    if created:
-        # principle amount
-        p = instance.amount
-        # monthly interset rate
-        i = instance.monthly_interset_rate
-        # term in months
-        n = instance.term
+def calculate_loan_month_payment(sender, instance: Loan, **kwargs):
+    # principle amount
+    p = instance.amount
+    # monthly interset rate
+    i = instance.monthly_interset_rate
+    # term in months
+    n = instance.term
 
-        # calculate the monthly payment using amortization formula
-        payment_amount = p * (i * (1 + i) ** n / ((1 + i) ** n - 1))
+    # calculate the monthly payment using amortization formula
+    payment_amount = round(p * (i * (1 + i) ** n / ((1 + i) ** n - 1)))
 
-        # update the monthly payment amount
-        instance.payment_amount = round(payment_amount)
-        # update the payment date which is one month from the loan start date
-        payment_date = instance.start_date + relativedelta(months=1)
-        instance.payment_date = payment_date
-        instance.save()
+    # update the monthly payment amount
+    instance.payment_amount = payment_amount
 
 
 @receiver(post_save, sender=PaymentTransaction)
@@ -49,6 +44,7 @@ def create_and_update_amortization(
             )
             principal_amount = payment_amount - interset_amount
             ending_balance = initial_balance - principal_amount
+            ending_balance = ending_balance if ending_balance > 0 else 0
 
             current_amortization = Amortization(
                 initial_balance=initial_balance,
@@ -56,10 +52,10 @@ def create_and_update_amortization(
                 interset_amount=interset_amount,
                 principal_amount=principal_amount,
                 ending_balance=ending_balance,
-                loan=loan
+                loan=loan,
             )
             current_amortization.save()
-            if current_amortization.ending_balance == 0:
+            if current_amortization.ending_balance <= 0:
                 loan.status == "PAID"
                 loan.save()
 
@@ -69,14 +65,14 @@ def create_and_update_amortization(
             interset_amount = initial_balance * loan.monthly_interset_rate
             principal_amount = payment_amount - interset_amount
             ending_balance = initial_balance - principal_amount
-
+            ending_balance = ending_balance if ending_balance > 0 else 0
             Amortization.objects.create(
                 initial_balance=initial_balance,
                 payment_amount=payment_amount,
                 interset_amount=interset_amount,
                 principal_amount=principal_amount,
                 ending_balance=ending_balance,
-                loan=loan
+                loan=loan,
             )
         loan.payment_date = loan.payment_date + relativedelta(months=1)
         loan.save()
